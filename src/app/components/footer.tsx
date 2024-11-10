a5904835682930cc328f433f8bee6ad78b6b34bc
ishk-maheen <maheenrashidsheikh786@gmail.com>
2024-11-10 22:14:36 +0500

    first commit

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p>Copyright &copy; 2024 - All rights reserved.</p>
+            <p>Designed by <span className="footerpara"><a href="https://www.linkedin.com/in/ishkmaheen24/"> Maheen Sheikh</a></span></p>
+        </div>
+    )
+}
---
+++ src/app/components/main.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import mainMyPic from "../data/Main pic.png"
+export default function Main() {
+    return (
+        <div className="main">
+            <div className="maindetail">
+                <h1 className="mainHeading">Hello It&apos;s me</h1>
+                <h1 className=" mainMainHeading"><span> Maheen Sheikh</span> </h1>
+                <h2 className="mainHeadings">And I&apos;m a <span> Frontend Developer</span> </h2>
+                <p className="mainpara">Creative and dedicated Frontend Developer, crafting responsive and engaging user experiences.</p>
+                <a href="https://www.linkedin.com/in/ishkmaheen24/"><button className="leftButton">Hire me</button></a>
+                <a href="https://github.com/ishk-maheen"><button className="rightButton">See Projects</button></a>
+            </div>
+            <div className="mypic">
+                <Image className="picture"
+                    src={mainMyPic}
+                    alt={"maheen"}
+                    width={400}
+                    height={400}
+                />
+            </div>
+        </div>
+    )
+}
